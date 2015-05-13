5a14dfc9d40eb594823d237dfc6a16537937a6dd
Keity Hashimoto <keity.mieko@automasistemas.com.br>
2015-05-13 15:16:36 -0700

    Initial Commit

---
+++ MimosMulher.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MimosMulher.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestMimosMulher
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MimosMulher.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MimosMulher.LojaVirtual.Web.MvcApplication" Language="C#" %>
